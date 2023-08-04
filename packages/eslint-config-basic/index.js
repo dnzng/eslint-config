51a19d2ddbb832176b99922b4884969981de7e05
dnzng <zhangchengxin@dylan.local>
2023-08-04 19:42:53 +0800

    feat(eslint-config-basic): add some commonly used rules for the yaml file

--- packages/eslint-config-basic/index.js
+++ packages/eslint-config-basic/index.js
@@ -148,5 +148,8 @@ module.exports = {
   rules: {
     'import/no-named-as-default': 'off',
     'import/no-named-as-default-member': 'off',
+
+    'yml/quotes': ['error', { prefer: 'single', avoidEscape: false }],
+    'yml/no-empty-document': 'off',
   }
 }
